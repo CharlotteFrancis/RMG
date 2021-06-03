bd773b13ecd116ca5e7e5df653a099d40043a327
Charlotte Wong <ChiharuTaru@gmail.com>
2021-06-03 15:07:38 -0700

    added newlines to markdown

--- md.js
+++ md.js
@@ -2,19 +2,20 @@ const md = {
   body: [],
 
   addh1 (string) {
-    this.body.push('# ' + string)
+    this.body.push('# ' + string + '\n') // either new line character or <br> here. figure it out later
   },
   addh2 (string) {
-    this.body.push('## ' + string)
+    this.body.push('## ' + string + '\n')
   },
   addh3 (string) {
-    this.body.push('### ' + string)
+    this.body.push('### ' + string + '\n')
   },
   addh4 (string) {
-    this.body.push('#### ' + string)
+    this.body.push('#### ' + string + '\n')
   },
   add (string) {
-    this.body.push(string)
+    this.body.push(string + '\n')
+    this.body.push('---' + '\n')
   },
   create () {
     let final = ''
@@ -30,8 +31,14 @@ const md = {
   section (s1, s2) {
     this.addh2(s1)
     this.add(s2)
+  },
+  blockSection (s1, s2) {
+    this.addh2(s1)
+    this.add('> ' + s2)
   }
-
 }
+
+// md.title('hello ', 'world')
+// console.log(md.create())
 
 module.exports = md
