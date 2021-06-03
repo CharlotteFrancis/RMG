fdab5d8340616a5a27cefa59427381448333cf31
Charlotte Wong <ChiharuTaru@gmail.com>
2021-06-03 09:39:17 -0700

    init npm and git ignore

--- app.js
+++ app.js
@@ -3,4 +3,7 @@
 // table of contents, installation, usage, license, contributing, tests, questions
 // title -> sections
 // github username links to pf
-// license gets a badge(?) 
+// license gets a badge(?)
+
+const md = require('./md.js')
+
