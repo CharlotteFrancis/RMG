086577955c92816394ae0e5ef01e96f048203b3b
Charlotte Wong <ChiharuTaru@gmail.com>
2021-06-03 15:07:13 -0700

    implemented prompt for section choices

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # RMG
 README Generator (RMG) generates a README.md based on user input
+
+##### Table of Contents  
+[Headers](#headers)  
+[Emphasis](#emphasis)  
+
+## Headers
+
+## Emphasis
+
--- app.js
+++ app.js
@@ -6,4 +6,90 @@
 // license gets a badge(?)
 
 const md = require('./md.js')
+const inquirer = require('inquirer')
+// const { listenerCount } = require('events')
 
+// ToC
+const tableOfContents = _ => {
+  // table stuff
+  // lodash kebab case
+}
+
+// Generic Section
+const addSection = (type) => {
+  //
+}
+
+const askSection = _ => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'type',
+      message: 'What type of section do you want to add?',
+      choices: [
+        'Table of Contents',
+        'Installation',
+        'Usage',
+        'License',
+        'Contributing',
+        'Tests',
+        'Questions'
+      ]
+    }
+  ])
+    .then((answers) => {
+      // process answers code here
+      // ask again
+      ask()
+    })
+    .catch(err => console.log(err))
+}
+
+// add the title
+const askTitle = _ => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of the project?'
+    },
+    {
+      type: 'input',
+      name: 'desc',
+      message: 'Give a description of the project'
+    }
+  ])
+    .then((answers) => {
+      // then stuff goes here
+      console.log(answers.title)
+      console.log(answers.desc)
+      md.title(answers.title, answers.desc)
+      // test output
+      console.log(md.create())
+      // add another
+      ask()
+    })
+    .catch(err => console.log(err))
+}
+
+// add a section?
+const ask = _ => {
+  inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'continue',
+      message: 'Would you like to add another section?'
+    }
+  ])
+    .then((cont) => {
+      if (cont.continue) {
+        // add section code
+        askSection()
+      } else {
+        // output writefile
+      }
+    })
+    .catch(err => console.log(err))
+}
+
+askTitle()
